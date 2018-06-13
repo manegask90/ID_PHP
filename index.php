24860b480f7f9c342880617ad0f88a991c60a779
Vitalik <vitalikmihaylishin@gmail.com>
2018-06-13 18:33:11 +0300

    done

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 $storage = [78, 4, 0, 6, 55, 13, 107, 55, 12, 32, 1, 5, 12, 78, 89];
+
+foreach ($storage as $value) {
+    if ($value % 2 == 0) {
+        echo $value;
+        echo '<br>';
+    }
+}
